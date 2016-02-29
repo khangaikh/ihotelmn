83a1d17528a888627d9f5b0164b8316957a100c8
khangai <khangai@alchemist.mn>
2016-02-29 21:04:11 +0800

    bug

--- req.php
+++ req.php
@@ -48,7 +48,7 @@
 			];
 
 			$ch = curl_init();
-			$url = "http://rs.ihotel/".$hotel->get("rs_name")."/api.php";
+			$url = "http://rs.ihotel.mn/".$hotel->get("rs_name")."/api.php";
 			echo $url;
 			curl_setopt($ch, CURLOPT_URL,$url);
 			curl_setopt($ch, CURLOPT_POST, 1);
