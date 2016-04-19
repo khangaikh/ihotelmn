d261331bdaf15b1c6d898774163661ffd438aa53
khangai <khangai@alchemist.mn>
2016-04-19 19:20:26 +0900

    Bug

--- index.php
+++ index.php
@@ -934,27 +934,27 @@
 
                     if (isset($_SERVER['HTTP_CLIENT_IP'])){
                         $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-                        echo $ipaddress;
+                        
                     }
                     if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
                         $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-                        echo $ipaddress;
+                      
                     }
                     if(isset($_SERVER['HTTP_X_FORWARDED'])){
                         $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-                        echo $ipaddress;
+                 
                     }
                     if(isset($_SERVER['HTTP_FORWARDED_FOR'])){
                         $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-                        echo $ipaddress;
+                 
                     }
                     if(isset($_SERVER['HTTP_FORWARDED'])){
                         $ipaddress = $_SERVER['HTTP_FORWARDED'];
-                        echo $ipaddress;
+      
                     }
                     if(isset($_SERVER['REMOTE_ADDR'])){
                         $ipaddress = $_SERVER['REMOTE_ADDR'];
-                        echo $ipaddress;
+                       
                     }
             
                     $query = new ParseQuery("_User");
