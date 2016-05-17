61bb489b8c94764e1bd4b42e56acc4ded01fbbee
khangai <khangai@alchemist.mn>
2016-05-17 16:18:58 +0900

    bug

--- js/detail/asemdetail.js
+++ js/detail/asemdetail.js
@@ -159,8 +159,6 @@ $(document).ready(function() {
         var enda   = new Date(end);
         var diff = new Date(enda - starta);
 
-        var t = $(this).attr('roomid');
-        $('select[roomid=t'+'--2'+']').hide();
 
         // get days
         var days = diff/1000/60/60/24;
